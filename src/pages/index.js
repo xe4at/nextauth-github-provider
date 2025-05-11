28cb7ae3f4134265702d5140bb41fa4249fff245
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-05-11 22:08:55 +0330

    Displaying elements on the site based on authentication

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Home() {
+  const { status } = useSession();
+
   const signInHandler = () => {
     signIn("github");
   };
@@ -13,14 +15,24 @@ export default function Home() {
   return (
     <div>
       <h1>Next-auth | GitHub provider</h1>
-      <button onClick={signInHandler}>Sign In</button>
-      <button onClick={signOutHandler}>Sign Out</button>
-      <button>
-        <Link href="/dashboard">Dashboard</Link>
-      </button>
-      <button>
-        <Link href="/ssrdashboard">SSR Dashboard</Link>
-      </button>
+
+      {status === "unauthenticated" && (
+        <button onClick={signInHandler}>Sign In</button>
+      )}
+
+      {status === "authenticated" && (
+        <>
+          <button onClick={signOutHandler}>Sign Out</button>
+
+          <Link href="/dashboard">
+            <button>Dashboard</button>
+          </Link>
+
+          <Link href="/ssrdashboard">
+            <button>SSR Dashboard</button>
+          </Link>
+        </>
+      )}
     </div>
   );
 }
